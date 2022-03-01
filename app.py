43d2eba783d1f81900efd2c79031e6c1882eb7c7
Kévin Duranty <duranty.kevin@gmail.com>
2022-03-01 11:21:20 +0100

    change app.py

--- app.py
+++ app.py
@@ -1,25 +1,42 @@
-from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
+
+
+import numpy as np
+import requests
+import pickle
+import cv2
+
+
+
+    
 app = Flask(__name__)
 
+def save_pred(path):
+    img = cv2.imread(f'data/{path}_leftImg8bit.png')
+    plt.imsave('./static/origine.png',img)
+    y = model.predict(np.array([img]))[0]
+    y = np.array([i.argmax() for i in y])*100
+    plt.imsave('./static/pred.png',y.reshape((512,1024)))
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+
+@app.route('/', methods=['GET'])
+def california_index():
+    return render_template("index.html")
 
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
+@app.route('/predict/', methods=['POST'])
+def result():
+    from tensorflow.keras.models import load_model
+    import matplotlib.pyplot as plt
+    
+    with open('model_pkl' , 'rb') as f:
+        model = pickle.load(f)
+    
+    if request.method == 'POST':
+                 name_img = request.form['name_img']
+                 save_pred(name_img)
+        
+    return render_template("prediction.html", name_img=name_img)
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
