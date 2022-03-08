435ac0d7e09f5f6a85b978aa7169c765ea814eaf
Kévin Duranty <duranty.kevin@gmail.com>
2022-03-08 18:07:26 +0100

    azure model

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
+# Sauvegarde du fichier data dans l'espace de stockage Azure
 from flask import Flask, render_template, request
+from tensorflow.keras.models import load_model
 
+from azureml.core import Workspace
+from azureml.core.model import Model
 
+from matplotlib.pyplot import imsave
+from PIL import Image
+from numpy import array
+import requests
 
-
-
-
-    
 app = Flask(__name__)
 
-def save_pred(path):
-    img = cv2.imread(f'data/{path}_leftImg8bit.png')
-    plt.imsave('./static/origine.png',img)
-    y = model.predict(np.array([img]))[0]
-    y = np.array([i.argmax() for i in y])*100
-    plt.imsave('./static/pred.png',y.reshape((512,1024)))
+# Connection à l'espace de travail d'Azure
+ws = Workspace(subscription_id="d5bb9744-4790-446f-b7e1-591e22995cc7",
+           resource_group="OpenClassrooms",
+           workspace_name="OC_IA")
+model = load_model(Model.get_model_path('Model_vgg_unet', _workspace=ws))
 
+def load_img_from_azure(name):
+    # Connection à l'espace de travail d'Azure
+    url = f'https://ocia0932039034.blob.core.windows.net/azureml-blobstore-f8554f92-a33d-430c-a1ff-4d9a166c55fc/UI/data/{name}_leftImg8bit.png'
+    X =  array(Image.open(requests.get(url, stream=True).raw))
+    imsave('./static/origine.png',X)
+    return X
 
 @app.route('/', methods=['GET'])
 def california_index():
@@ -22,22 +31,12 @@ def california_index():
 
 
 @app.route('/predict/', methods=['POST'])
-def result():
-
-    import numpy as np
-    import requests
-    import pickle
-    import cv2
-    from tensorflow.keras.models import load_model
-    import matplotlib.pyplot as plt
-    
-    with open('model_pkl' , 'rb') as f:
-        model = pickle.load(f)
-    
+def result():    
     if request.method == 'POST':
-                 name_img = request.form['name_img']
-                 save_pred(name_img)
-        
+        name_img = request.form['name_img']
+        X = array([load_img_from_azure(name_img)])
+        y = array([[i.argmax() for i in u] for u in model.predict(X[:])])
+        imsave('./static/pred.png',y.reshape((512,1024)))
     return render_template("prediction.html", name_img=name_img)
 
 if __name__ == '__main__':
